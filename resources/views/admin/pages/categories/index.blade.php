74e0461f80c932bd6ef146eeea2fa59a603874ac
Sahil5861 <sahilkhan05861@gmail.com>
2024-07-26 21:14:37 +0530

    commit done

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -107,7 +107,6 @@
     </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     $(document).ready(function () {
         var CategoryTable = $('#category-table').DataTable({
